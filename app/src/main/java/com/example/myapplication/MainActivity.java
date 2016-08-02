6f6ef73a7beeb5af0abf462b59127aa974a9f028
lenter85 <lenter1985@naver.com>
2016-08-02 13:39:24 +0900

    ㅋㅋ

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -35,6 +35,7 @@ public class MainActivity extends AppCompatActivity
     //준식커밋
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
